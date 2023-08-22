6b3d2d949989298ce1330bd742e17e77bae2d6f9
EdwardJhosep <1454668@senati.pe>
2023-08-22 10:33:18 -0500

    creacion del proyecto algoritmo

---
+++ algoritmos1/src/main/java/com/mycompany/algoritmos1/Algoritmos1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmos1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Algoritmos1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
