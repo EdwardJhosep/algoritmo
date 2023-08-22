f03c7787a8c9ef668e2a320f2a617a1af365c8a2
EdwardJhosep <1454668@senati.pe>
2023-08-22 10:46:09 -0500

    importar paquetes-crear del mapa-almacenamiento

--- algoritmos1/src/main/java/com/mycompany/algoritmos1/Algoritmos1.java
+++ algoritmos1/src/main/java/com/mycompany/algoritmos1/Algoritmos1.java
@@ -3,7 +3,10 @@
  */
 
 package com.mycompany.algoritmos1;
-
+//importamos JOptionPane-ingreso,Map-funcion,HashMap-almacenar
+import javax.swing.JOptionPane;
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author Administrador
@@ -11,6 +14,12 @@ package com.mycompany.algoritmos1;
 public class Algoritmos1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+  //cree una mapa para almacenar los datos 
+        Map<String, String> personas = new HashMap<>();
+        boolean exit = false;
+
+        while (!exit) {
     }
 }
+}
+
